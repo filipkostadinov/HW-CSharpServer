cc6b7f5f929a3d7936afe0afec926b21e70f5a27
filipkostadinov <fkostadinov1@gmail.com>
2019-09-19 21:49:13 +0200

    added homework

---
+++ SedcServer/ServerInterfaces/QueryCollection.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServerInterfaces
+{
+    public class QueryCollection
+    {
+        private readonly List<KeyValuePair<string,string>> data = new List<KeyValuePair<string, string>>();
+
+        public QueryCollection(List<KeyValuePair<string, string>> initialData)
+        {
+            data = initialData;
+        }
+
+        public static QueryCollection Empty
+        {
+            get
+            {
+                return new QueryCollection(new List<KeyValuePair<string, string>>());
+            }
+        }
+
+        public override string ToString()
+        {    
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < data.Count; i++)
+            {
+                sb.Append(i == data.Count - 1 ? $"{data[i].Key}={data[i].Value}" : $"{data[i].Key}={data[i].Value}&");
+            }
+            return sb.ToString();
+        }
+    }
+}
